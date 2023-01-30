77e86ba8cd144d0f7188fd7f7d96644a4d0eefa9
kurumi196 <ogawa.k196@gmail.com>
2023-01-30 13:35:59 +0900

    Revert "予約詳細ページにユーザー情報を表示する　対応完了しました"
    This reverts commit 94269a09068ee3fbcb1abc0c6d8b7a08e763f80a.

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -2,25 +2,23 @@
 
 @section('content')
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
-    <div class="w-50 m-auto h-75">
-        <p><span>{{$date}}日</span><span class="ml-3">{{$part}}部</span></p>
-        <div class="h-75 border">
-        <table class="">
-            <tr class="text-center">
-            <th class="w-25">ID</th>
-            <th class="w-25">名前</th>
-            </tr>
-            @foreach ($reservePersons as $reservePerson)
-                @foreach($reservePerson->users as $user)
-                <tr class="text-center">
-                <!-- ユーザー情報だけとってくる -->
-                <td class="w-25">{{$user->id}}</td>
-                <td class="w-25">{{$user->over_name}}{{$user->under_name}}</td>
-                </tr>
-                @endforeach
-            @endforeach
-        </table>
-        </div>
+  <div class="w-50 m-auto h-75">
+    <p><span>{{$date}}日</span><span class="ml-3">{{$part}}部</span></p>
+    <div class="h-75 border">
+      <table class="">
+        <tr class="text-center">
+          <th class="w-25">ID</th>
+          <th class="w-25">名前</th>
+        </tr>
+        @foreach ($reservePersons as $user)
+        <tr class="text-center">
+          <!-- ユーザー情報だけとってくる -->
+          <td class="w-25">{{$user->users}}</td>
+          <td class="w-25"></td>
+        </tr>
+        @endforeach
+      </table>
     </div>
+  </div>
 </div>
 @endsection
