931355b3ef4f1b5d450c1c717113e59338df94b7
kurumi196 <ogawa.k196@gmail.com>
2023-02-16 19:06:42 +0900

    レイアウト（予約詳細ページ）

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
     <div class="w-50 m-auto h-75">
-        <p><span>{{$date}}日</span><span class="ml-3">{{$part}}部</span></p>
-        <div class="h-75 border">
-        <table class="">
+        <p class="title"><span>{{$date}}日</span><span class="ml-3">{{$part}}部</span></p>
+        <div class="h-75 border p-2 rounded" style="background-color:#FFF;">
+        <table class="reserve_detail_text">
             <tr class="text-center">
                 <th class="w-25">ID</th>
                 <th class="w-25">名前</th>
@@ -15,7 +15,7 @@
             @foreach($reservePerson->users as $user)
             <tr class="text-center">
                 <td class="w-25">{{$user->id}}</td>
-                <td class="w-25">{{$user->over_name}}{{$user->under_name}}</td>
+                <td class="w-25">{{$user->over_name}} {{$user->under_name}}</td>
                 <td class="w-25">リモート</td>
             </tr>
             @endforeach
